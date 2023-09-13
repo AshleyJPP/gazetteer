711d9bb466f39307fb4b18c0e3dbe1345039065d
AshleyJPP <139067880+AshleyJPP@users.noreply.github.com>
2023-09-13 23:20:15 +0100

    Update latlngtocc.php

--- php/latlngtocc.php
+++ php/latlngtocc.php
@@ -1,75 +1,36 @@
 <?php
+$lat = urlencode($_REQUEST['lat']);
+$lng = urlencode($_REQUEST['lng']);
+$apiUrl = 'http://api.geonames.org/countryCodeJSON?lat=' . $lat . '&lng=' . $lng . '&type=JSON&username=ajppeters'; // Set type to JSON
+$ch = curl_init($apiUrl);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-    
-    ini_set('display_errors', 'On');
-    error_reporting(E_ALL);
+$countryCodeData = curl_exec($ch);
 
-    $executionStartTime = microtime(true);
-
-    // Initialize cURL
-    $lat = urlencode($_REQUEST['lat']);
-    $lng = urlencode($_REQUEST['lng']);
-    $apiUrl = 'http://secure.geonames.org/countryCodeJSON?lat=' . $lat . '&lng=' . $lng . '&username=ajppeters';
-    $ch = curl_init($apiUrl);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-    // Store the data
-    $countryData = curl_exec($ch);
-    
-    if ($countryData === false) {
+if ($countryCodeData === false) {
     $output['status']['code'] = "500";
     $output['status']['name'] = "internal server error";
     $output['status']['description'] = "Error executing cURL request: " . curl_error($ch);
 } else {
-    // Decode JSON response
-    $code = json_decode($countryData, true);
+        $countryCodeResult = json_decode($countryCodeData, true);
 
-    // Check if decoding was successful
     if (json_last_error() === JSON_ERROR_NONE) {
-        // Your normal processing code here...
+        $countryCode = $countryCodeResult['countryCode'];
+
+        $output['status']['code'] = "200";
+        $output['status']['name'] = "ok";
+        $output['status']['description'] = "success";
+        $output['data']['countryCode'] = $countryCode;
+        header('Content-Type: application/json; charset=UTF-8');
+        echo json_encode($output);
     } else {
         $output['status']['code'] = "500";
         $output['status']['name'] = "internal server error";
         $output['status']['description'] = "Error decoding JSON response: " . json_last_error_msg();
+        header('Content-Type: application/json; charset=UTF-8');
+        echo json_encode($output);
     }
-    
-    var_dump($countryData);
-    
-    // End the cURL
+
     curl_close($ch);
-
-    // Decode JSON response
-    $code = json_decode($countryData, true);
-
-    ////////////////////////////////////////////////
-
-    $ch1 = curl_init('http://secure.geonames.org/countryInfoJSON?formatted=true&country=' . $code['countryCode'] . '&username=ajppeters&style=full;');
-    curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true);
-
-    // Store the data
-    $currencyData = curl_exec($ch1);
-    
-    var_dump($currencyData);
-    
-    // End the cURL
-    curl_close($ch1);
-
-    // Decode JSON response
-    $currency = json_decode($currencyData, true);
-
-    ////////////////////////////////////////////////
-
-    // Assign the output variable properties to relevant data
-    $output['status']['code'] = "200";
-    $output['status']['name'] = "ok";
-    $output['status']['description'] = "success";
-    $output['status']['executedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-    $output['data']['country'] = $code['countryCode'];
-    $output['data']['currency'] = $currency['geonames'][0]['currencyCode'];
-
-    header('Content-Type: application/json; charset=UTF-8');
-
-   // Echo out all the useful data
-    echo json_encode($output);
-    
-}?>
+}
+?>
