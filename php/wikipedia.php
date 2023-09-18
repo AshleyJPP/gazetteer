db816c43ca9add111f464601d4df693e7c04d7bc
AshleyJPP <139067880+AshleyJPP@users.noreply.github.com>
2023-09-18 22:53:29 +0100

    Create wikipedia.php

---
+++ php/wikipedia.php
@@ -0,0 +1,20 @@
+<?php
+
+$lat = $_GET['lat'];
+$lng = $_GET['lng'];
+
+$url = "http://api.geonames.org/findNearbyWikipedia?lat={$lat}&lng={$lng}&username=ajppeters"; // Replace "demo" with your actual username
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+$xml = simplexml_load_string($result);
+$json = json_encode($xml);
+
+header('Content-Type: application/json');
+echo $json;
+
+?>
